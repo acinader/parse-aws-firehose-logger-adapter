4e3075241cd830640e8425c1e14ef50ae7ee6fa0
Arthur Cinader <arthur@sugarinc.com>
2016-07-22 16:17:36 -0400

    - validate options - more test cases

--- spec/firehoseLoggerAdapterSpec.js
+++ spec/firehoseLoggerAdapterSpec.js
@@ -3,12 +3,28 @@ const FirehoseLoggerAdapter = require('../lib/index.js').FirehoseLoggerAdapter;
 
 // If you want to actually send a test log, then set NODE_ENV to something
 // other than 'test'.  See spec/helpers
-describe('FirehoseLoggerAdapter tests', () => {
-  const adapter = new FirehoseLoggerAdapter('testing123', {
-    streamName: 'testing123',
-    firehoseOptions: {
-      region: 'us-east-1',
-    },
-  });
+// process.env.NODE_ENV = 'foo';
+describe('FirehoseLoggerAdapter adapter tests', () => {
+  const options = {
+    region: 'us-east-1',
+  };
+
+  const adapter = new FirehoseLoggerAdapter('testing123', options);
   loggerAdapterTests.testAdapter('FirehoseLoggerAdapter', adapter);
+});
+
+describe('FirehoseLoggerAdapter configuration tests', () => {
+  it('missing arguments throws', () => {
+    expect(() => new FirehoseLoggerAdapter()).toThrow();
+  });
+
+  it('can configure with string arg', () => {
+    expect(() => new FirehoseLoggerAdapter('test-stream').not.toThrow());
+  });
+
+  it('can configure with env var', () => {
+    process.env.FIREHOSE_LOGGER_STREAM_NAME = 'test-stream';
+    expect(() => new FirehoseLoggerAdapter({ name: 'test3' })).not.toThrow();
+    delete process.env.FIREHOSE_LOGGER_STREAM_NAME;
+  });
 });
--- src/FirehoseLoggerAdapter.js
+++ src/FirehoseLoggerAdapter.js
@@ -1,17 +1,56 @@
-const WinstonLoggerAdapter = require('parse-server').WinstonLoggerAdapter;
+import { WinstonLoggerAdapter } from 'parse-server/lib/Adapters/Logger/WinstonLoggerAdapter';
 const WinstonFirehose = require('winston-firehose');
-console.log(WinstonLoggerAdapter);
+
+const DEFAULT_REGION = 'us-east-1';
 
 export class FirehoseLoggerAdapter extends WinstonLoggerAdapter {
-  constructor(streamName, firehoseOptions) {
-    super();
+  constructor(...args) {
+    super(args);
+    const options = this.optionsFromArguments(args);
+
     if (process.env.NODE_ENV !== 'test') {
-      const transport = new WinstonFirehose(firehoseOptions);
+      // FIXME: use the mock class in winston-firehose instead....
+      const transport = new WinstonFirehose(options);
       this.addTransport(transport);
     }
+  }
+
+  // following three functions copied from s3Adapter.  Would be nice to have a lib...
+  requiredOrFromEnvironment(options, key, env) {
+    options[key] = options[key] || process.env[env]; // eslint-disable-line no-param-reassign
+
+    if (!options[key]) {
+      throw new Error(`FirehoseLoggerAdapter requires option '${key}' or env. variable ${env}`);
+    }
+    return options;
+  }
+
+  fromEnvironmentOrDefault(options, key, env, defaultValue) {
+    // eslint-disable-next-line no-param-reassign
+    options[key] = options[key] || process.env[env] || defaultValue;
+    return options;
+  }
+
+  optionsFromArguments(args) {
+    let options = {};
+    const streamNameOrOptions = args[0];
+    if (typeof streamNameOrOptions === 'string') {
+      options.streamName = streamNameOrOptions;
+    } else {
+      options = streamNameOrOptions || {};
+    }
+    options = this.requiredOrFromEnvironment(options, 'streamName', 'FIREHOSE_LOGGER_STREAM_NAME');
+
+    let firehoseOptions = options.firehoseOptions || {};
+    firehoseOptions = this.fromEnvironmentOrDefault(
+      firehoseOptions, 'region', 'FIREHOSE_LOGGER_REGION', DEFAULT_REGION);
+    firehoseOptions = this.fromEnvironmentOrDefault(
+      firehoseOptions, 'level', 'FIREHOSE_LOGGER_LEVEL', '');
+    options.firehoseOptions = firehoseOptions;
+
+    return options;
   }
 }
 
 export default FirehoseLoggerAdapter;
 module.exports = FirehoseLoggerAdapter;
-
