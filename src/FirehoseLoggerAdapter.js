b59afdaa60d441af716ec3a263e39208f6af70e1
Arthur Cinader <arthur@sugarinc.com>
2016-10-11 11:14:05 -0700

    Make changes to pass lint: - use this in objects.  much better

--- src/FirehoseLoggerAdapter.js
+++ src/FirehoseLoggerAdapter.js
@@ -1,4 +1,5 @@
 import { WinstonLoggerAdapter } from 'parse-server/lib/Adapters/Logger/WinstonLoggerAdapter';
+
 const WinstonFirehose = require('winston-firehose');
 
 const DEFAULT_REGION = 'us-east-1';
@@ -6,47 +7,46 @@ const DEFAULT_REGION = 'us-east-1';
 export class FirehoseLoggerAdapter extends WinstonLoggerAdapter {
   constructor(arg) {
     super(arg);
-    const options = this.optionsFromArguments(arg);
+    this.optionsFromArguments(arg);
 
     if (process.env.NODE_ENV !== 'test') {
-      const transport = new WinstonFirehose(options);
+      const transport = new WinstonFirehose(this.options);
       this.addTransport(transport);
     }
   }
 
   // following three functions copied from s3Adapter.  Would be nice to have a lib...
-  requiredOrFromEnvironment(options, key, env) {
-    options[key] = options[key] || process.env[env]; // eslint-disable-line no-param-reassign
+  requiredOrFromEnvironment(key, env) {
+    // eslint-disable-next-line no-param-reassign
+    this.options[key] = this.options[key] || process.env[env];
 
-    if (!options[key]) {
+    if (!this.options[key]) {
       throw new Error(`FirehoseLoggerAdapter requires option '${key}' or env. variable ${env}`);
     }
-    return options;
+    return this.options;
   }
 
-  fromEnvironmentOrDefault(options, key, env, defaultValue) {
+  fromEnvironmentOrDefault(key, env, defaultValue) {
     // eslint-disable-next-line no-param-reassign
-    options[key] = options[key] || process.env[env] || defaultValue;
-    return options;
+    this.options[key] = this.options[key] || process.env[env] || defaultValue;
+    return this.options;
   }
 
   optionsFromArguments(streamNameOrOptions) {
-    let options = {};
+    this.options = {};
     if (typeof streamNameOrOptions === 'string') {
-      options.streamName = streamNameOrOptions;
+      this.options.streamName = streamNameOrOptions;
     } else {
-      options = streamNameOrOptions || {};
+      this.options = streamNameOrOptions || {};
     }
-    options = this.requiredOrFromEnvironment(options, 'streamName', 'FIREHOSE_LOGGER_STREAM_NAME');
+    this.options = this.requiredOrFromEnvironment('streamName', 'FIREHOSE_LOGGER_STREAM_NAME');
 
-    let firehoseOptions = options.firehoseOptions || {};
+    let firehoseOptions = this.options.firehoseOptions || {};
     firehoseOptions = this.fromEnvironmentOrDefault(
       firehoseOptions, 'region', 'FIREHOSE_LOGGER_REGION', DEFAULT_REGION);
     firehoseOptions = this.fromEnvironmentOrDefault(
       firehoseOptions, 'level', 'FIREHOSE_LOGGER_LEVEL', '');
-    options.firehoseOptions = firehoseOptions;
-
-    return options;
+    this.options.firehoseOptions = firehoseOptions;
   }
 }
 
